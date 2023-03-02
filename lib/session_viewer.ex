756dfdddaf2667f854dece042b3f0fddc8977f05
Carlos Ramirez <calbertora@gmail.com>
2023-03-02 10:04:07 -0500

    Initial commit

---
+++ lib/session_viewer.ex
@@ -0,0 +1,18 @@
+defmodule SessionViewer do
+  @moduledoc """
+  Documentation for `SessionViewer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SessionViewer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/session_viewer_test.exs
@@ -0,0 +1,8 @@
+defmodule SessionViewerTest do
+  use ExUnit.Case
+  doctest SessionViewer
+
+  test "greets the world" do
+    assert SessionViewer.hello() == :world
+  end
+end
